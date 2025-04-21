6757255c2b3496a4157641f8122563314235d6fd
Kreskowl <kreskow1@gmail.com>
2025-04-21 09:25:14 +0300

    fulfill coverage requirements 2

--- server/src/test/java/ru/practicum/shareit/integration/itemIntegrationTest/ItemServiceTest.java
+++ server/src/test/java/ru/practicum/shareit/integration/itemIntegrationTest/ItemServiceTest.java
@@ -3,17 +3,16 @@ package ru.practicum.shareit.integration.itemIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import ru.practicum.shareit.integration.IntegrationTestBase;
 import ru.practicum.shareit.ShareItServer;
-import ru.practicum.shareit.dto.item.CommentCreateDto;
-import ru.practicum.shareit.dto.item.CommentDto;
-import ru.practicum.shareit.dto.item.ItemCreateDto;
-import ru.practicum.shareit.dto.item.ItemResponseDto;
-import ru.practicum.shareit.dto.item.ItemUpdateDto;
+import ru.practicum.shareit.booking.BookingService;
+import ru.practicum.shareit.dto.booking.BookingCreateDto;
+import ru.practicum.shareit.dto.item.*;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.exception.ValidationException;
+import ru.practicum.shareit.integration.IntegrationTestBase;
 import ru.practicum.shareit.item.ItemService;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,12 +22,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class ItemServiceTest extends IntegrationTestBase {
     @Autowired
     private ItemService service;
+    @Autowired
+    private BookingService bookingService;
     ItemCreateDto newItem;
+    CommentCreateDto comment;
 
     @Test
     void shouldSaveToDB() {
-        newItem = new ItemCreateDto("screwdriver", "used 2 times",
-                getSecondUser().getId(), false, null);
+        newItem = new ItemCreateDto("screwdriver", "used 2 times", getSecondUser().getId(), false, null);
         ItemResponseDto result = service.createItem(newItem);
 
         assertThat(itemRepository.findAll()).hasSize(2);
@@ -64,8 +65,7 @@ public class ItemServiceTest extends IntegrationTestBase {
 
     @Test
     void shouldDeleteFromDB() {
-        newItem = new ItemCreateDto("screwdriver", "used 2 times",
-                getSecondUser().getId(), false, null);
+        newItem = new ItemCreateDto("screwdriver", "used 2 times", getSecondUser().getId(), false, null);
         ItemResponseDto result = service.createItem(newItem);
         service.deleteById(getItem().getId());
 
@@ -78,22 +78,27 @@ public class ItemServiceTest extends IntegrationTestBase {
 
     @Test
     void shouldGetAllItemsByOwnerId() {
-        newItem = new ItemCreateDto("screwdriver", "used 2 times",
-                getUser().getId(), false, null);
-        service.createItem(newItem);
+        newItem = new ItemCreateDto("screwdriver", "used 2 times", getUser().getId(), true, null);
+        ItemResponseDto saved = service.createItem(newItem);
+
+        BookingCreateDto futureBooking = new BookingCreateDto(saved.getId(), LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(2));
+        bookingService.createBooking(futureBooking, getSecondUser().getId());
+
+        comment = new CommentCreateDto("Pretty good");
+        service.addComment(getItem().getId(), getSecondUser().getId(), comment);
+
+        BookingCreateDto pastBooking = new BookingCreateDto(saved.getId(), LocalDateTime.now().minusDays(2), LocalDateTime.now().minusDays(1));
         List<ItemResponseDto> result = service.findAllByOwnerId(1);
         System.out.println("Returned items: " + result.size());
         System.out.println("Owner id: " + getUser().getId());
         assertThat(itemRepository.findAll()).hasSize(2);
 
-        assertThat(result)
-                .extracting(ItemResponseDto::getName)
-                .containsExactlyInAnyOrder("Hammer", "screwdriver");
+        assertThat(result).extracting(ItemResponseDto::getName).containsExactlyInAnyOrder("Hammer", "screwdriver");
     }
 
     @Test
     void shouldAddCommentToItemInDB() {
-        CommentCreateDto comment = new CommentCreateDto("Pretty good");
+        comment = new CommentCreateDto("Pretty good");
         CommentDto result = service.addComment(getItem().getId(), getSecondUser().getId(), comment);
 
         assertThat(result.getItemId()).isEqualTo(getItem().getId());
@@ -116,10 +121,7 @@ public class ItemServiceTest extends IntegrationTestBase {
     void shouldThrowIfUpdateByNotOwner() {
         ItemUpdateDto update = new ItemUpdateDto("Updated", "Updated desc", true, getSecondUser().getId());
 
-        NotFoundException notFound = assertThrows(
-                NotFoundException.class,
-                () -> service.update(getSecondUser().getId(), getItem().getId(), update)
-        );
+        NotFoundException notFound = assertThrows(NotFoundException.class, () -> service.update(getSecondUser().getId(), getItem().getId(), update));
 
         assertThat(notFound.getMessage()).contains("not the owner");
     }
@@ -128,10 +130,7 @@ public class ItemServiceTest extends IntegrationTestBase {
     void shouldThrowIfUserDidNotBookItem() {
         CommentCreateDto comment = new CommentCreateDto("Sneaky comment");
 
-        ValidationException ex = assertThrows(
-                ValidationException.class,
-                () -> service.addComment(getItem().getId(), getUser().getId(), comment)
-        );
+        ValidationException ex = assertThrows(ValidationException.class, () -> service.addComment(getItem().getId(), getUser().getId(), comment));
 
         assertThat(ex.getMessage()).contains("has not completed a booking");
     }
@@ -185,5 +184,4 @@ public class ItemServiceTest extends IntegrationTestBase {
 
         assertThat(result).isEmpty();
     }
-
 }
